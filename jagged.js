da90243ee6b31189644da1c19bb325ef89d94508
hasibulislam999 <hasib143sl@gmail.com>
2022-05-18 22:07:07 +0600

    jagged bug fix done

--- jagged.js
+++ jagged.js
@@ -1,32 +1,8 @@
 // Find the maximum number in a jagged array given below array of numbers?
 
-/* for (let i = 1; i < ar.length; i++) { // outer loop
-    for (let j = 0; j < ar[i].length; j++) { // inner loop 1
-        if (ar[i][j].length > 1) {
-            for (let k = 0; k < ar[i][j].length; k++) { // inner loop 2
-                if (max < ar[i][j][k]) {
-                    max = ar[i][j][k];
-                }
-            }
-        } else {
-            if (max < ar[i][j]) {
-                max = ar[i][j];
-            }
-        }
-    }
-} */
-
-// an alternative both works same!
-
 function jagged(arr) {
     for (const number of arr) {
-        if (Array.isArray(number)) {
-            jagged(number);
-        } else {
-            if (max < number) {
-                max = number;
-            }
-        }
+        Array.isArray(number) ? jagged(number) : (max < number) && (max = number);
     }
 }
 
