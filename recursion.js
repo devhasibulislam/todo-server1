1abb2f08e545835da16deb6ba176df093e75b2b2
hasibulislam999 <hasib143sl@gmail.com>
2022-05-18 21:42:32 +0600

    recursion bug fix done

--- recursion.js
+++ recursion.js
@@ -1,10 +1,12 @@
 // Create a function that will find the factorial number of [9] using recursion.
+
 function recursion(number) {
-    if (number === 1) {
+    if (number === 1 || number === 0) {
         return 1;
     } else {
         return number * recursion(number - 1);
     }
 }
 
-console.log(recursion(5));
+const fact = recursion(0);
+console.log(fact);
