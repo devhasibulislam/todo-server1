f2358932e3bb8febb7f00b7569587ac3ff311939
hasibulislam999 <hasib143sl@gmail.com>
2022-05-19 00:11:39 +0600

    adding and complete js core tasks

--- factorial.js
+++
@@ -1,8 +0,0 @@
-// Create a function that will find the factorial number of [9] using recursion.
-
-function factorial(number) {
-    return (number === 1 || number === 0) ? 1 : number * factorial(number - 1);
-}
-
-const fact = factorial(9);
-console.log(fact);
--- recursion.js
+++
@@ -1,8 +0,0 @@
-// Create a function that will find the factorial number of [9] using recursion.
-
-function recursion(number) {
-    return (number === 1 || number === 0) ? 1 : number * recursion(number - 1);
-}
-
-const fact = recursion(9);
-console.log(fact);
